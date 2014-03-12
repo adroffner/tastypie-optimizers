ab8e0793821c1fe268ec4f1f9f1bce8447a22a33
CentOS 6.5 <adroffner@localhost.localdomain>
2014-03-11 22:36:42 -0700

    Add more comments explaining example suite.

--- tastypie_optimizers/models.py
+++ tastypie_optimizers/models.py
@@ -1,3 +1,5 @@
+""" Sample Models demonstrate the optimized resources.
+"""
 from django.db import models
 
 class USState(models.Model):
@@ -9,14 +11,16 @@ class USState(models.Model):
 
 
 class StarRating(models.Model):
-    """ Star Ratings measure numerically, id IN(1, 2, ...)i, with PK.
+    """ Star Ratings measure numeric id IN(1, 2, ...), with Django's default PK.
     The PK appears as a human readable number in the foreign key.
     """
     rating = models.CharField(max_length=100)
 
 
 class Hotel(models.Model):
-    """ Hotel has both kind of FK to relate.
+    """ Hotel has both kinds of FK as related fields.
+    Its HotelModelResource avoids needless SQL queries
+    for `state` and `rating` REST responses.
     """
     name = models.CharField(max_length=100)
     state = models.ForeignKey(USState)
